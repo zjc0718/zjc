0b3d7c049196b1629e91b489d06b2f1ff6c9ec8d
hp <hp>
2017-11-10 14:11:17 +0800

    frist

---
+++ python/yuanzu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5, 6, 7 )
+#tup1[0] = 100   修改元组，非法操作
+#del tup1          删除元组，非法操作
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+
+
+print ('abc', -4.24e93, 18+6.6j, 'xyz')
+x, y = 1, 2
+print ("Value of x , y : ", x,y)

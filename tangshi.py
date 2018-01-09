674058d953683745ece7878779fd356c1cb0c320
zjc0718 <33245424+zjc0718@users.noreply.github.com>
2018-01-09 09:25:36 +0800

    Add files via upload

--- tangshi.py
+++ tangshi.py
@@ -1,26 +1,20 @@
-#本程序实现的功能是爬取唐诗三百首，并存在本地的TXT文件中。
-
-#导入相应的模块
-import urllib.request
+import urllib2
 import re
-
-#访问唐诗三百首的服务器，并使用正则表达式爬取每首古诗的链接。
 url = "http://www.gushiwen.org/gushi/tangshi.aspx"
-data = urllib.request.urlopen(url).read().decode("utf-8", "ignore")
-pat = '<span><a href="(.*?)"'
-rst = re.compile(pat, re.S).findall(data)
-
-#打开一个本地文件目录，用于存放爬取的古诗。
-fh = open("D:/test1/gushi.txt", "w")
-#依次访问每首古诗的链接，并使用正则表达式抓取古诗内容
+data = urllib2.urlopen(url).read().decode("utf-8","ignore")
+pat = re.compile('<span><a href="(.*?)"',re.S)
+rst = re.findall(pat,data)
 for i in range(0,len(rst)):
-    url_0 = rst[i]
-    data_0 = urllib.request.urlopen(url_0).read().decode("utf-8", "ignore")
-    pat_0 = '<div class="left">.*?<div style=".*?">.*?<textarea style=".*?">(.*?)h'
-    gushi = re.compile(pat_0, re.S).findall(data_0)
-    #将爬取到的古诗写入本地文件。
-    fh.write(gushi[0]+"\n\n")
-    print("正在写入...")
-#最后关闭文件。
-fh.close()
-print("写入完成！！！")
+    url2 = rst[i]
+    data2 = urllib2.urlopen(url2).read().decode("utf-8","ignore")
+    pat2 = re.compile('<textarea style=" .*?>(.*?)h',re.S)
+    rst2 = re.findall(pat2,data2)
+    rst3 = rst2[0].encode("utf-8")
+    print rst3
+    print '\n'
+    if (i+1)%5 == 0:
+        input = raw_input()
+        if input == '':
+            continue
+        else:
+            break
